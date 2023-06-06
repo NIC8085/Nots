def2f62e0a029163a2b818ee8d5cdadafeaa09a2
Kacper <kacper.markowski2@teb.edu.pl>
2023-06-06 19:35:20 +0200

    dodanie kolumn do tabeli priority

--- NotsApp/admin.py
+++ NotsApp/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import Note, Priority
 
 # Register your models here.
+
+
+@admin.register(Note)
+class NoteAdmin(admin.ModelAdmin):
+    list_display = ('title', 'priority', 'author', 'created', 'updated')
+    search_fields = ('title', 'content')
+    ordering = ('priority', 'updated', 'created', 'title')
+
+
+@admin.register(Priority)
+class PriorityAdmin(admin.ModelAdmin):
+    list_display = ('title', 'shortcut', 'color', 'weight')
+    search_fields = ('title', 'color', 'weight')
+    ordering = ('weight', 'title', 'color')
--- NotsApp/models.py
+++ NotsApp/models.py
@@ -1,3 +1,4 @@
+from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
 
 # Create your models here.
@@ -5,16 +6,20 @@ from django.db import models
 
 class Priority(models.Model):
     title = models.CharField(max_length=20)
+    shortcut = models.CharField(max_length=15, default='')
     color = models.CharField(max_length=15)
+    weight = models.IntegerField(default=0, validators=[MaxValueValidator(6), MinValueValidator(0)])
 
     priorityManager = models.Manager()
 
+    def __str__(self):
+        return self.title
 
 class Note(models.Model):
     title = models.CharField(max_length=30)
     content = models.TextField()
-    created = models.DateTimeField(auto_now_add=True)
-    updated = models.DateTimeField(auto_now=True)
+    created = models.DateField(auto_now_add=True)
+    updated = models.DateField(auto_now=True)
     author = models.CharField(max_length=50)
     priority = models.ForeignKey(Priority, on_delete=models.CASCADE, related_name='notes')
 
