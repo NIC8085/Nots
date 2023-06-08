2da8dfa54dc4a4f2939d9fcdfa4a004eec2c67ce
Kacper <kacper.markowski2@teb.edu.pl>
2023-06-08 19:33:11 +0200

    naprawienie niedziałającego sortowania oraz dodanie paginacji

--- NotsApp/static/js/script.js
+++ NotsApp/static/js/script.js
@@ -1,12 +1,12 @@
 document.getElementById('priority').addEventListener('change', function() {
-            var selectedOption = this.options[this.selectedIndex];
-            var priority = selectedOption.value;
-            var currentUrl = window.location.href;
+        var selectedOption = this.options[this.selectedIndex];
+        var priority = selectedOption.value;
+        var currentUrl = window.location.href;
 
-            if (currentUrl.includes('priority=')) {
-                currentUrl = currentUrl.replace(/priority=\w+/, 'priority=' + priority);
-            } else {
-                currentUrl += (currentUrl.includes('?') ? '&' : '?') + 'priority=' + priority;
-            }
-            window.location.href = currentUrl;
-        });
+        if (currentUrl.includes('priority=')) {
+            currentUrl = currentUrl.replace(/priority=\w+/, 'priority=' + priority);
+        } else {
+            currentUrl += (currentUrl.includes('?') ? '&' : '?') + 'priority=' + priority;
+        }
+        window.location.href = currentUrl;
+});
--- NotsApp/views.py
+++ NotsApp/views.py
@@ -1,27 +1,23 @@
 from django.shortcuts import redirect
 from django.views.generic import ListView, DetailView, CreateView, UpdateView
 from .models import Note
-from django.core.paginator import Paginator, EmptyPage
 
 
 class NoteView(ListView):
     model = Note
     template_name = 'web/home.html'
-    paginate_by = 10  # Określa ilość notatek na stronie
+    paginate_by = 10
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        notes = context['object_list']
-        paginator = Paginator(notes, self.paginate_by)
-        page_number = self.request.GET.get('page')
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        priority = self.request.GET.get('priority', 'descending')
 
-        try:
-            page_obj = paginator.get_page(page_number)
-        except EmptyPage:
-            page_obj = paginator.get_page(1)  # Wyświetl pierwszą stronę, jeśli numer strony jest nieprawidłowy
+        if priority == 'ascending':
+            queryset = queryset.order_by('priority__weight')
+        else:
+            queryset = queryset.order_by('-priority__weight')
 
-        context['page_obj'] = page_obj
-        return context
+        return queryset
 
 
 class NoteDetailView(DetailView):
